12e97d40928f4b4c298a077b0093db645738ccaf
priverop <priverop@users.noreply.github.com>
2026-04-09 18:05:54 +0200

    tests(api): fixed namespace

---
+++ test/controllers/api/movies_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class MoviesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @movie = movies(:one)
+  end
+
+  test "should get index" do
+    get api_movies_url, as: :json
+    assert_response :success
+  end
+
+  test "should create movie" do
+    assert_difference("Movie.count") do
+      post api_movies_url, params: { movie: { duration: @movie.duration, genre: @movie.genre, name: @movie.name } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show movie" do
+    get api_movie_url(@movie), as: :json
+    assert_response :success
+  end
+
+  test "should update movie" do
+    patch api_movie_url(@movie), params: { movie: { duration: @movie.duration, genre: @movie.genre, name: @movie.name } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy movie" do
+    assert_difference("Movie.count", -1) do
+      delete api_movie_url(@movie), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
--- test/controllers/api/theaters_controller_test.rb
+++ test/controllers/api/theaters_controller_test.rb
@@ -6,31 +6,31 @@ class TheatersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get theaters_url, as: :json
+    get api_theaters_url, as: :json
     assert_response :success
   end
 
   test "should create theater" do
     assert_difference("Theater.count") do
-      post theaters_url, params: { theater: { location: @theater.location, name: @theater.name, price: @theater.price, discounted_price: @theater.discounted_price, discounted_days: @theater.discounted_days } }, as: :json
+      post api_theaters_url, params: { theater: { location: @theater.location, name: @theater.name, price: @theater.price, discounted_price: @theater.discounted_price, discounted_days: @theater.discounted_days } }, as: :json
     end
 
     assert_response :created
   end
 
   test "should show theater" do
-    get theater_url(@theater), as: :json
+    get api_theater_url(@theater), as: :json
     assert_response :success
   end
 
   test "should update theater" do
-    patch theater_url(@theater), params: { theater: { location: @theater.location, name: @theater.name, price: @theater.price, discounted_price: @theater.discounted_price, discounted_days: @theater.discounted_days } }, as: :json
+    patch api_theater_url(@theater), params: { theater: { location: @theater.location, name: @theater.name, price: @theater.price, discounted_price: @theater.discounted_price, discounted_days: @theater.discounted_days } }, as: :json
     assert_response :success
   end
 
   test "should destroy theater" do
     assert_difference("Theater.count", -1) do
-      delete theater_url(@theater), as: :json
+      delete api_theater_url(@theater), as: :json
     end
 
     assert_response :no_content
