49d417a3670fcae900fb05b7684f67a9e2ecd6b2
priverop <priverop@users.noreply.github.com>
2026-04-27 11:33:38 +0200

    feat(scraper): add logger

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+require "logger"
+
 module Scraper
+  def self.logger
+    @logger ||= defined?(Rails) ? Rails.logger : Logger.new($stdout)
+  end
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
   class InvalidUrlError < StandardError; end
   class MoviesNotFoundError < StandardError; end
+  class CalendarNotFoundError < StandardError; end
+  class UnknownLanguageError < StandardError; end
 end
