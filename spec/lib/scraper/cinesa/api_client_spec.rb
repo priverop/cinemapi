c7f5ccdb413750c97643d2513edde73e0ee7fd49
priverop <priverop@users.noreply.github.com>
2026-05-01 13:16:11 +0200

    test(scraper.cinesa): add test for ApiClient

---
+++ spec/lib/scraper/cinesa/api_client_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../../../../lib/scraper'
+require_relative '../../../../lib/scraper/cinesa/api_client'
+
+RSpec.describe Scraper::Cinesa::ApiClient do
+  let(:calendar_url) { "https://vwc.cinesa.es/WSVistaWebClient/ocapi/v1/film-screening-dates" }
+  let(:headers) { { "Authorization" => "Bearer test-token" } }
+  let(:client) { described_class.new(headers, "027") }
+
+  describe "#data" do
+    context "with a successful response", vcr: { cassette_name: "cinesa/api_client/data_200" } do
+      it "returns the response body and appends siteIds to the URL" do
+        result = client.data(calendar_url)
+        expect(result).to include("filmScreeningDates")
+      end
+    end
+
+    context "with an error response", vcr: { cassette_name: "cinesa/api_client/data_401" } do
+      it "returns the body without raising" do
+        result = client.data(calendar_url)
+        expect(result).to include("Unauthorized")
+      end
+    end
+  end
+end
