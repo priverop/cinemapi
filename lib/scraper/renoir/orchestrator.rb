54a8d238f72609e731d0de3fdd928c1530b8f779
priverop <priverop@users.noreply.github.com>
2026-05-01 12:56:56 +0200

    chore(scraper.renoir.orchestrator): bad indentation

--- lib/scraper/renoir/orchestrator.rb
+++ lib/scraper/renoir/orchestrator.rb
@@ -13,28 +13,28 @@ module Scraper
 
           Scraper.logger.info("Starting scrape for #{base_url}.")
 
-            main_html = Scraper::Client.read(base_url)
-            calendar_days = Scraper.logger.tagged("CalendarParser") { Scraper::Renoir::CalendarParser.new(main_html).days }
+          main_html = Scraper::Client.read(base_url)
+          calendar_days = Scraper.logger.tagged("CalendarParser") { Scraper::Renoir::CalendarParser.new(main_html).days }
 
-            calendar_days.each do |day|
-              url = day[:url]
-              date = day[:date]
-              Scraper.logger.tagged(date) do
-                begin
-                  new_url = base_url.merge(url)
-                  html = Scraper::Client.read(new_url)
-                  parsed = Scraper.logger.tagged("MovieParser") { Scraper::Renoir::MovieParser.new(html).parse }
-                  normalized = Scraper.logger.tagged("Normalizer") { Scraper::Renoir::Normalizer.new(date).normalize(parsed) }
-                  Scraper.logger.tagged("Importer") { Scraper::Importer.new(theater:).import(normalized) }
-                  days_ok += 1
-                rescue => e
-                  days_failed += 1
-                  Scraper.logger.error("Day #{date} failed: #{e.class}: #{e.message}.")
-                end
+          calendar_days.each do |day|
+            url = day[:url]
+            date = day[:date]
+            Scraper.logger.tagged(date) do
+              begin
+                new_url = base_url.merge(url)
+                html = Scraper::Client.read(new_url)
+                parsed = Scraper.logger.tagged("MovieParser") { Scraper::Renoir::MovieParser.new(html).parse }
+                normalized = Scraper.logger.tagged("Normalizer") { Scraper::Renoir::Normalizer.new(date).normalize(parsed) }
+                Scraper.logger.tagged("Importer") { Scraper::Importer.new(theater:).import(normalized) }
+                days_ok += 1
+              rescue => e
+                days_failed += 1
+                Scraper.logger.error("Day #{date} failed: #{e.class}: #{e.message}.")
               end
             end
+          end
 
-            Scraper.logger.info("Done. days_ok=#{days_ok} days_failed=#{days_failed}.")
+          Scraper.logger.info("Done. days_ok=#{days_ok} days_failed=#{days_failed}.")
         end
       end
     end
