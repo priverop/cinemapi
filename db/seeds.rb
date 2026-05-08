abb7d637b36474edb621c985e0a5be26eeac1019
priverop <priverop@users.noreply.github.com>
2026-05-08 11:22:36 +0200

    refactor(seeds): remove movies/showtimes (parser will do), updated theaters

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,32 +12,9 @@ User.destroy_all
 
 puts 'Creating Theaters'
 puts '- - - - - - - - - -'
-princesa = Theater.create!(name: 'Cine Princesa', location: 'Plaza de los Cubos, Madrid', price: 8.9, discounted_price: 4.9, discounted_days: [ 'monday', 'wednesday' ])
-plaza = Theater.create!(name: 'Cine Plaza España', location: 'Calle Paralela, Madrid', price: 8.9, discounted_price: 4.9, discounted_days: [ 'monday', 'wednesday' ])
-pio = Theater.create!(name: 'Cinesa Príncipe Pío', location: 'Centro Comercial Principe Pío, Madrid', price: 11.5, discounted_price: 6.5, discounted_days: [ 'wednesday' ])
-
-puts 'Creating Movies'
-puts '- - - - - - - - - -'
-due = Movie.create!(title: 'Due Date', duration: 90)
-gone = Movie.create!(title: 'Gone Girl', duration: 150)
-weapons = Movie.create!(title: 'Weapons', duration: 110)
-
-puts 'Creating Showtimes'
-puts '- - - - - - - - - -'
-Showtime.create!(theater: princesa, movie: due, showtime: DateTime.parse('2025-12-15 19:00'))
-Showtime.create!(theater: princesa, movie: due, showtime: DateTime.parse('2025-12-15 22:30'))
-Showtime.create!(theater: plaza, movie: due, showtime: DateTime.parse('2025-12-15 17:30'))
-Showtime.create!(theater: pio, movie: due, showtime: DateTime.parse('2025-12-15 23:00'))
-
-Showtime.create!(theater: princesa, movie: gone, showtime: DateTime.parse('2025-12-15 20:30'))
-Showtime.create!(theater: plaza, movie: gone, showtime: DateTime.parse('2025-12-15 19:30'))
-Showtime.create!(theater: pio, movie: gone, showtime: DateTime.parse('2025-12-15 18:00'))
-Showtime.create!(theater: pio, movie: gone, showtime: DateTime.parse('2025-12-15 22:00'))
-
-Showtime.create!(theater: princesa, movie: weapons, showtime: DateTime.parse('2025-12-15 20:30'))
-Showtime.create!(theater: princesa, movie: weapons, showtime: DateTime.parse('2025-12-15 23:30'))
-Showtime.create!(theater: plaza, movie: weapons, showtime: DateTime.parse('2025-12-15 23:30'))
-Showtime.create!(theater: pio, movie: weapons, showtime: DateTime.parse('2025-12-15 23:30'))
+princesa = Theater.create!(name: 'Cine Princesa', location: 'Plaza de los Cubos, Madrid', price: 8.9, discounted_price: 4.9, discounted_days: [ 'monday', 'wednesday' ], website: "https://www.cinesrenoir.com/cine/cines-princesa/cartelera/", scraper_key: 1)
+plaza = Theater.create!(name: 'Cine Plaza España', location: 'Calle Martín de los Heros, Madrid', price: 8.9, discounted_price: 4.9, discounted_days: [ 'monday', 'wednesday' ], website: "https://www.cinesrenoir.com/cine/renoir-plaza-de-espana/cartelera/", scraper_key: 1)
+pio = Theater.create!(name: 'Cinesa Príncipe Pío', location: 'Centro Comercial Principe Pío, Madrid', price: 11.5, discounted_price: 6.5, discounted_days: [ 'wednesday' ], website: "https://www.cinesa.es/cines/principe-pio/", scraper_key: 2, scraper_external_id: "027")
 
 puts 'Creating Users'
 puts '- - - - - - - - - -'
